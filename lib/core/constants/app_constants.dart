25ff9406ff705dd254d7059ee3e971049e81f989
amir bakhtiari <amir.bakhtiari@ymail.com>
2026-04-19 15:31:10 +0330

    Create app_constants.dart with application constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  static const String appName = 'Flutter POS System';
+  static const String version = '1.0.0';
+  static const String apiUrl = 'https://api.flutterpos.com';
+  static const String supportEmail = 'support@flutterpos.com';
+  // Add more constants as needed
+}
