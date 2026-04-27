724374da64835b4907346a00f9207f4c82ea0822
amir bakhtiari <amir.bakhtiari@ymail.com>
2026-04-27 19:36:48 +0330

    ایجاد فایل سبک‌های متن

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,40 @@
+/// توالی‌های متن‌ها
+class سبک_متن {
+  // سبک‌های عنوان
+  static const عنوان_بزرگ = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static const عنوان_متوسط = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+  );
+  
+  static const عنوان_کوچک = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  );
+  
+  // سبک‌های بدن
+  static const متن_بزرگ = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static const متن_متوسط = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.normal,
+  );
+  
+  static const متن_کوچک = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+  
+  // سبک‌های برچسب
+  static const برچسب = TextStyle(
+    fontSize: 13,
+    fontWeight: FontWeight.w500,
+  );
+}
